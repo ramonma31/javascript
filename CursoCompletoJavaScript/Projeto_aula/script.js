cc459a240562bc7c757602201d6230c0040574fc
Dev_Moreira <ramonma31@gmail.com>
2023-05-14 12:57:27 -0300

    att aulas

--- CursoCompletoJavaScript/Projeto_aula/script.js
+++ CursoCompletoJavaScript/Projeto_aula/script.js
@@ -20,7 +20,6 @@ function addNavImage() {
     })
   }
 }
-addNavImage()
 
 function initFaq() {
   const acordionlist = document.querySelectorAll(".js-acordion dt")
@@ -39,14 +38,51 @@ function initFaq() {
   });
 }
 
-initFaq()
+function scrollsuave() {
+  
+  const linksInterno = document.querySelectorAll(".js-menu a[href^='#']");
+  function scrolltosection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute("href")
+    const section = document.querySelector(href)
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    })
+    /*window.scrollTo({
+      top: section,
+      behavior: "smooth"
+    });*/
+  };
 
-const linksInterno = document.querySelectorAll(".js-menu a[href^='#']");
-
-function scrolltosection() {
-  this.preventDefault()
+  linksInterno.forEach(link => {
+    link.addEventListener("click", scrolltosection);
+  })
 }
 
-linksInterno.forEach(link => {
-  link.addEventListener("click", scrolltosection);
-})
+function scrollinitanimation() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if(sections.length){
+    const windowmetade = innerHeight * 0.8;
+
+    function animascroll() {
+      sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const issectionvisible = (sectionTop - windowmetade) < 0;
+        if(issectionvisible) {
+          section.classList.add("active")
+        }
+      })
+    }
+  }
+
+  animascroll()
+
+  window.addEventListener("scroll", animascroll)
+}
+
+
+addNavImage()
+initFaq()
+scrollsuave()
+scrollinitanimation()
